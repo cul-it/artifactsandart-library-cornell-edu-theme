03a51d6cb5ee0ba4108981f8954e7ce11cc2f085
Melissa Wallace <mhk33@cornell.edu>
2012-06-28 19:07:17 +0000

    git-svn-id: https://svn.library.cornell.edu/artifactsandarttheme/trunk@34 2a97d85c-e2e3-4be1-82da-b1ed9b33a2f6

--- artifactsandart/page--front.tpl.php
+++ artifactsandart/page--front.tpl.php
@@ -46,8 +46,38 @@
 				<?php if(drupal_is_front_page()) {unset($page['content']['system_main']['default_message']);} ?>
 			</div>
 			<div class="four columns clearfix">
-				<?php print render($page['sidebar_first']); ?>
- 				<?php print render($page['sidebar_second']); ?>
+				<div id="myCarousel" class="carousel slide">
+				  	<!-- Carousel items -->
+				  	<div class="carousel-inner">
+				    	<div class="active item">
+				    		<img src="img/slideshow/1.jpg" alt="">
+							<div class="carousel-caption">
+								<p>Caption 1 here</p>
+							</div>
+				    	</div>
+					    <div class="item">
+							<img src="img/slideshow/2.jpg" alt="">
+							<div class="carousel-caption">
+								<p>Caption 2 here</p>
+							</div>
+						</div>
+					    <div class="item">
+					    	<img src="img/slideshow/3.jpg" alt="">
+							<div class="carousel-caption">
+								<p>Caption 3 here</p>
+							</div>
+					    </div>
+						<div class="item">
+					    	<img src="img/slideshow/4.jpg" alt="">
+							<div class="carousel-caption">
+								<p>Caption 4 here</p>
+							</div>
+					    </div>
+				  	</div>
+				  	<!-- Carousel nav -->
+				  	<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+				  	<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+				</div>
 			</div>
 		</div>
 	</div><!-- main-content -->	
@@ -55,3 +85,6 @@
 		<?php print render($page['footer']); ?>
 	</footer
 </div><!-- container -->
+<script src="js/jquery-1.7.2.min.js"></script>
+<script src="js/bootstrap-carousel.js"></script>
+<script src="js/bootstrap-transition.js"></script>

6de94f1bc36f9d467dc02e696ea16ca3be2a35fe
Melissa Wallace <mhk33@cornell.edu>
2012-06-30 00:37:58 +0000

    git-svn-id: https://svn.library.cornell.edu/artifactsandarttheme/trunk@46 2a97d85c-e2e3-4be1-82da-b1ed9b33a2f6

--- page.tpl.php
+++ page.tpl.php
@@ -95,27 +95,9 @@
 		<h1 class="title"><a href="<?php print $front_page; ?>">Campus Artifacts <span>&amp;</span> Art</a></h1>
 	</header>
 	
-	<?php if ($main_menu): ?>
 	<nav>
-		<?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
 		<?php print render($page['navigation']); ?>
-     </nav>
-     <?php endif; ?>
+    </nav>
 	
 	<div class="main-content clearfix">
 		<div class="gutter">

f66a61fa91b0bfa923b66377333973f2657863df
Melissa Wallace <mhk33@cornell.edu>
2012-06-30 00:47:04 +0000

    git-svn-id: https://svn.library.cornell.edu/artifactsandarttheme/trunk@49 2a97d85c-e2e3-4be1-82da-b1ed9b33a2f6

--- html.tpl.php
+++ html.tpl.php
@@ -86,7 +86,8 @@
 	<![endif]-->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php print $page_top; ?>
   <?php print $page; ?>
-
+  <?php print $page_bottom; ?>
 </body>
 </html>

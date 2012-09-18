c58198d24c8a0a230ac375d9e6e3d70ddfa8987a
Melissa Wallace <mhk33@cornell.edu>
2012-09-18 17:28:28 +0000

    git-svn-id: https://svn.library.cornell.edu/artifactsandarttheme/trunk@116 2a97d85c-e2e3-4be1-82da-b1ed9b33a2f6

--- maintenance-page.tpl.php
+++ maintenance-page.tpl.php
@@ -1,76 +1,34 @@
 <?php
+
 /**
  * @file
- * Zen theme's implementation to display a single Drupal page.
+ * Default theme implementation to display a single Drupal page while offline.
  *
- * Available variables:
- *
- * General utility variables:
- * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
- * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
- * - $is_front: TRUE if the current page is the front page.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $is_admin: TRUE if the user has permission to access administration pages.
- *
- * Site identity:
- * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or
- *   prefix.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
- *
- * Navigation:
- * - $main_menu (array): An array containing the Main menu links for the
- *   site, if they have been configured.
- * - $secondary_menu (array): An array containing the Secondary menu links for
- *   the site, if they have been configured.
- * - $secondary_menu_heading: The title of the menu used by the secondary links.
- * - $breadcrumb: The breadcrumb trail for the current page.
- *
- * Page content (in order of occurrence in the default page.tpl.php):
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title: The page title, for use in the actual HTML content.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
- * - $messages: HTML for status and error messages. Should be displayed
- *   prominently.
- * - $tabs (array): Tabs linking to any sub-pages beneath the current page
- *   (e.g., the view and edit tabs when displaying a node).
- * - $action_links (array): Actions local to the page, such as 'Add menu' on the
- *   menu administration interface.
- * - $feed_icons: A string of all feed icons for the current page.
- * - $node: The node object, if there is an automatically-loaded node
- *   associated with the page, and the node ID is the second argument
- *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
- *   comment/reply/12345).
- *
- * Regions:
- * - $page['header']: Items for the header region.
- * - $page['navigation']: Items for the navigation region, below the main menu (if any).
- * - $page['help']: Dynamic help text, mostly for admin pages.
- * - $page['highlighted']: Items for the highlighted content region.
- * - $page['content']: The main content of the current page.
- * - $page['sidebar_first']: Items for the first sidebar.
- * - $page['sidebar_second']: Items for the second sidebar.
- * - $page['footer']: Items for the footer region.
- * - $page['bottom']: Items to appear at the bottom of the page below the footer.
+ * All the available variables are mirrored in html.tpl.php and page.tpl.php.
+ * Some may be blank but they are provided for consistency.
  *
  * @see template_preprocess()
- * @see template_preprocess_page()
- * @see zen_preprocess_page()
- * @see template_process()
+ * @see template_preprocess_maintenance_page()
  */
 ?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
 
-
+<head>
+	<meta charset="utf-8">
+  	<title><?php print $head_title; ?></title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  	<?php print $styles; ?>
+  	<?php print $scripts; ?>
+  	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
 <div class="cu-identity">
 	<div class="container">
 		<div id="cu-identity-wrap">
@@ -128,8 +86,6 @@
 
         <div class="gutter">
 
-			<?php print $messages; ?>
-			<?php print render($tabs); ?>
 			<?php if ($title): ?>
 		        <h2><?php print $title; ?></h2>
 		    <?php endif; ?>
@@ -159,3 +115,6 @@
 </div><!-- container -->
 
 <?php print render($page['bottom']); ?>
+
+</body>
+</html>
